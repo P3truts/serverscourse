8b64a811c8fcfa9a3918219e62936fc026a4e018
P3truts <obreja.petrut@gmail.com>
2026-05-01 14:56:44 +0300

    A: add startup structure

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+const port = 8080;
+app.use(express.static("./app"));
+app.use("/app", express.static("./src/app"));
+//app.get("/app", (req, res) => {
+//    res.sendFile("src/app/logo.png", { root: "./src/app" });
+//});
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+app.get("/healthz", handlerReadiness);
+async function handlerReadiness(req, resp) {
+    resp.set("Content-Type", "text/plain; charset=utf-8");
+    resp.send("OK");
+}
