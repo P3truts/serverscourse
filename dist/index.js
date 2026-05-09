67de7697a19bf1e52e4594ae35e2674cf0d9efa7
P3truts <obreja.petrut@gmail.com>
2026-05-09 12:02:21 +0300

    B: add middleware

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,17 @@
 import express from "express";
+import { middlewareLogResponses } from "./middleware.js";
 const app = express();
 const port = 8080;
 app.use(express.static("./app"));
 app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
 //app.get("/app", (req, res) => {
 //    res.sendFile("src/app/logo.png", { root: "./src/app" });
 //});
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
-app.get("/healthz", handlerReadiness);
+app.get("/healthz", middlewareLogResponses, handlerReadiness);
 async function handlerReadiness(req, resp) {
     resp.set("Content-Type", "text/plain; charset=utf-8");
     resp.send("OK");
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { Request, Response } from "express";
+import { middlewareLogResponses } from "./middleware.js";
 
 const app = express();
 const port = 8080;
 
 app.use(express.static("./app"));
 app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
 //app.get("/app", (req, res) => {
 //    res.sendFile("src/app/logo.png", { root: "./src/app" });
 //});
@@ -15,7 +17,7 @@ app.listen(port, () => {
 });
 
 
-app.get("/healthz", handlerReadiness);
+app.get("/healthz", middlewareLogResponses, handlerReadiness);
 
 async function handlerReadiness(req: Request, resp: Response) {
     resp.set("Content-Type", "text/plain; charset=utf-8");
